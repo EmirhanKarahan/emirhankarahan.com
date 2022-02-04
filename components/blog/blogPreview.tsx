7ac3eca3210f342b2250d9abd06fa14f09dcc5e1
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2022-02-05 00:17:43 +0300

    Update BlogPreview to show contentful content

--- components/blog/blogPreview.tsx
+++ components/blog/blogPreview.tsx
@@ -1,13 +1,25 @@
 import { ComponentProps } from "react";
+import format from "date-fns/format";
+import { useRouter } from "next/router";
+import { tr, enUS } from "date-fns/locale";
 
-const BlogPreview = ({ title, subtitle, date, link }: ComponentProps<any>) => {
+const BlogPreview = ({ blogPost }: ComponentProps<any>) => {
+  let { locale } = useRouter();
+  const dateLocale = locale === "en" ? enUS : tr;
+
   return (
     <article>
-      <a href={link} target={"_blank"} rel="noopener noreferrer">
-        <h2 className="text-xl font-bold hover:text-blue-600">{title}</h2>
-        <p>{subtitle}</p>
+      <a href="/blogPreviewDeneme" target={"_blank"} rel="noopener noreferrer">
+        <h2 className="text-xl font-bold hover:text-blue-600">
+          {blogPost.fields.title}
+        </h2>
+        <p>{blogPost.fields.subtitle}</p>
         <div className="text-gray-400">
-          <time>{date}</time>
+          <time>
+            {format(new Date(blogPost.sys.createdAt), "MMM dd, yyyy", {
+              locale: dateLocale,
+            })}
+          </time>
         </div>
       </a>
     </article>
