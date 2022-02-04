e941e4dd877c7bc37d90dba25a647abf34d6a44f
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2022-02-05 00:17:17 +0300

    Add contentful support to get blog posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,37 @@
-import type { NextPage } from "next";
-import { useRouter } from "next/router";
+import type { GetStaticPropsContext, NextPage } from "next";
+import { createClient } from "contentful";
+
 import BlogPreview from "../components/blog/blogPreview";
 import Transition from "../components/transition";
-import en from "../locales/en/blog";
-import tr from "../locales/tr/blog";
 
-const Blog: NextPage = () => {
-  const { locale } = useRouter();
-  const blogPosts = locale === "en" ? en : tr;
-
+const Blog: NextPage<any> = ({ blogPosts }) => {
   return (
     <Transition>
       <section className="site-container space-y-4">
-        <BlogPreview {...blogPosts[0]}></BlogPreview>
+        {blogPosts.map((blogPost: any) => (
+          <BlogPreview key={blogPost.sys.id} blogPost={blogPost} />
+        ))}
       </section>
     </Transition>
   );
 };
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID!,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+  });
+
+  const res = await client.getEntries({
+    content_type: "blogPost",
+    locale: context.locale,
+  });
+
+  return {
+    props: {
+      blogPosts: res.items,
+    },
+  };
+}
 
 export default Blog;
