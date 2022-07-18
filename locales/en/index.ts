7eaffb1b832cd849bf8a6a06cad823e73d16130b
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2022-07-18 15:39:16 +0300

    Update locales

--- locales/en/index.ts
+++ locales/en/index.ts
@@ -1,12 +1,12 @@
 export default {
   siteTitle: "Emirhan KARAHAN",
   siteDescription:
-    "Personal website of Emirhan KARAHAN. I'm trying to specialized in front-end. With the help of modern technologies, i build web and mobile apps which users enjoy.",
+    "Personal website of Emirhan KARAHAN. I'm trying to specialized in IOS development. With the help of modern technologies, i build web and mobile apps which users enjoy.",
   navBlog: "Blog",
   navResume: "Resume",
   navPortfolio: "Porfolio",
   headerTitle: "It's Emirhan KARAHAN",
   headerSubtitle: "Software Engineering Candidate",
   headerDescription:
-    "Trying to specialized in front-end. With the help of modern technologies, i build web and mobile apps which users enjoy.",
+    "Trying to specialized in IOS development. With the help of modern technologies, i build web and mobile apps which users enjoy.",
 };
