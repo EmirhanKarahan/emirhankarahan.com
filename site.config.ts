80b16e4ada756c8e9cbb5a02c6de4649443f07ec
Emirhan Karahan <emirhan.karahan@asiselektronik.com.tr>
2024-08-30 16:37:01 +0300

    Create emirhankarahan.com

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'ebbd2d3829f4469284fe941f5c9a564c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'emirhankarahan.com',
+  domain: 'emirhankarahan.com',
+  author: 'Emirhan Karahan',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Emirhan Karahan: iOS developer, blogger, and tech enthusiast. Explore my projects, read my latest insights, and get in touch.',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'emirhankarahan_',
+  github: 'emirhankarahan',
+  linkedin: 'emirhankarahan',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
